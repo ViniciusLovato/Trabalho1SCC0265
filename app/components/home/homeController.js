1ee1477d637942b4a93caa49010d8ad3a586ef8d
mogsilva <mogsilva@yahoo.com.br>
2015-05-06 16:15:45 -0300

    Fazendo validacao

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,3 +1,8 @@
 angular.module('HomeApp').controller('HomeCtrl', function ($scope) {
+	$scope.$on("$viewContentLoaded", function() {
+		$('.collapsible').collapsible({
+			accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+		});
+	});
     $scope.message = 'Everyone come and see how good I look!';
 });
--- app/components/userRegister/userRegisterController.js
+++ app/components/userRegister/userRegisterController.js
@@ -5,14 +5,15 @@ angular.module('UserRegisterApp').controller('UserRegisterCtrl', function ($scop
     $scope.userRegister = {};
 
     $scope.requirement = function(field){
-        return (field.$error.required && field.$dirty);
+		return field.$error.required && field.$dirty;
+		
     }
     
     $scope.registerUser = function () {
 
-        //If any of the fields are not filled, then set dirty to show the erros
+        //If any of the fields are not filled, then set dirty to show the errors
         angular.forEach($scope.form.$error.required, function (field) {
-            field.$setDirty();
+            field.$setTouched();
         });
 
         console.log($scope.form.$valid);
